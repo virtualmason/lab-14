a1f4328dad4bfdbab14ab617143b52e8c2f91a32
Floyd Orr <floydcanhelp@gmail.com>
2019-05-03 19:17:21 -0500

    finished project

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # lab-14
-Number and name of feature: ________________________________
+Number and name of feature: ______SQL commands__________________________
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: __1 hours___
 
-Start time: _____
+Start time: __5:00PM___
 
-Finish time: _____
+Finish time: __7:00pm took my time to go over it well___
 
-Actual time needed to complete: _____
+Actual time needed to complete: _2 hours____
---
+++ data/migrations/timestamp-description.sql
@@ -0,0 +1,24 @@
+Query 1: CREATE TABLE BOOKSHELVES (id SERIAL PRIMARY KEY, name VARCHAR(255));
+
+This query will create a second table in the lab14_normal database named bookshelves. Confirm the success of this command by typing \d bookshelves in your SQL shell. You should see the bookshelves table schema, as shown above.
+Query 2: INSERT INTO bookshelves(name) SELECT DISTINCT bookshelf FROM books;
+1556928384987
+This query will use a simple subquery to retrieve unique bookshelf values from the books table and insert each one into the bookshelves table in the name column. This is a great pattern for copying lots of data between tables.
+Confirm the success of this command by typing SELECT COUNT(*) FROM bookshelves; in your SQL shell. The number should be greater than zero.
+Query 3: ALTER TABLE books ADD COLUMN bookshelf_id INT;
+1556928389087
+This query will add a column to the books table named bookshelf_id. This will connect each book to a specific bookshelf in the other table.
+Confirm the success of this command by typing \d books in your SQL shell. The table schema should now include a column for bookshelf_id, in addition to the column for the string bookshelf; the bookshelf column will be removed in Query 5.
+Query 4: UPDATE books SET bookshelf_id=shelf.id FROM (SELECT * FROM bookshelves) AS shelf WHERE books.bookshelf = shelf.name;
+1556928387534
+This query will prepare a connection between the two tables. It works by running a subquery for every row in the books table. The subquery finds the bookshelf row that has a name matching the current book’s bookshelf value. The id of that bookshelf row is then set as the value of the bookshelf_id property in the current book row.
+Confirm the success of this command by typing SELECT bookshelf_id FROM books; in your SQL shell. The result should display a column containing the unique ids for the bookshelves. The numbers should match the total number returned from Query 2 when you confirmed the success of the insertion of names into the bookshelves table.
+Query 5: ALTER TABLE books DROP COLUMN bookshelf;
+1556928384902
+This query will modify the books table by removing the column named bookshelf. Now that the books table contains a bookshelf_id column which will become a foreign key, your table does not need to contain a string representing each bookshelf.
+Confirm the success of this command by typing \d books in your SQL shell. The books table schema should be updated to reflect the schema provided above, without the bookshelf column.
+Query 6: ALTER TABLE books ADD CONSTRAINT fk_bookshelves FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id);
+1556928384452
+This query will modify the data type of the bookshelf_id in the books table, setting it as a foreign key which references the primary key in the bookshelves table. Now PostgreSQL knows HOW these 2 tables are connected.
+Confirm the success of this command by typing /d books in your SQL shell. You should see details about the foreign key constraints, as shown in the schema above.
+1556928384637
